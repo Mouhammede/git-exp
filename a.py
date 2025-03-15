5c1a69d89db6a08215497ab76e481771a8617193
mouhamed <righi.mohammedtahar@gmail.com>
2025-03-15 13:47:38 +0100

    v2

--- a.py
+++ a.py
@@ -2,9 +2,11 @@ import math
 print("change 1")
 if(math.exp(math.pi)>5):
     k="yyy"
-else:
+elif(math.exp(math.pi)<5):
     # forgot what this das
     k="fff"
     #23/02/2025 sumthing is not right
+else:
+    k="eee"
 i=int(k)
 print(i+" "+k)
